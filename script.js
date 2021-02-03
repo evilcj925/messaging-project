e8f3f3dfb8c15069ea4f53d8a8bcbaa455139dd3
evilcj925 <cjkindred925@gmail.com>
2021-02-03 05:45:38 -0800

    First Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+/*make random messaging app; focus will be astrology predictor 
+start with making making array of signs, luck, and sayings*/
+
+let signs = ['Dragon', 'Moon', 'Sun', 'Tiger', 'Hare']
+let luck = ["high", "low", "hard to read"]
+let phrase = ["trust no one", "look for a friend in need", "be ready for change", "hold fast to your values"]
+
+//make functions to call selections from arrays randomly 
+
+function tellFortune= ()=> {
+    const randomSign = signs[Math.floor(Math.random() * array.length)];
+    const randomLuck = luck[Math.floor(Math.random() * array.length)];
+    const randomPhrase = phrase[Math.floor(Math.random() * array.length)];
+    //above creates random choice from the arrays and returns them  as random*
+    
+    //interpolate them all together 
+
+
+}
